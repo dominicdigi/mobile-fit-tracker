37f1f52b877e796d0dc9e6518ec337433a3a33f1
Dominic DiGiovanni <dominicadigiovanni@gmail.com>
2023-01-28 15:30:13 -0600

    adding in redux for login

--- App.js
+++ App.js
@@ -22,44 +22,12 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import CustomWebview from './src/components/webview/CustomWebview';
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-
-import axios from 'axios';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Login from './src/components/login/Login.jsx';
+import store from './src/redux/store';
+import { Provider } from 'react-redux';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -70,102 +38,13 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  GoogleSignin.configure({
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-    webClientId: '935356266275-gbp522qsjmtns3ujt84ulgo60vatvan6.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-    offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    hostedDomain: '', // specifies a hosted domain restriction
-    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-    accountName: '', // [Android] specifies an account name on the device that should be used
-    iosClientId: '935356266275-gbkb2n1g354k4mbavda6mgfpq8pcr1vj.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-    googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
-    openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
-    profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
-  });
-
-  // TODO Create login component
-  const signIn = async () => {
-    try {
-      var userInfo = await GoogleSignin.signIn();
-      // TODO 
-      // save access and refresh token in local storage
-      if(userInfo && userInfo.idToken){
-        userInfo = {"idToken": userInfo.idToken, "user": userInfo.user ? userInfo.user : ''};
-        axios({
-          method: 'post',
-          url: 'http://localhost:3001/auth/google/verify',
-          data: userInfo
-        }).then(res => {
-          var tokens = res;
-        }).catch(function (error){
-            console.log(error);
-        });
-      }
-      else {
-        // show message to user that login was unsuccesful
-      }
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (e.g. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
-      } else {
-        // some other error happened
-      }
-    }
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          {/* {showLoginWebView ? <CustomWebview></CustomWebview> : ''} */}
-          <GoogleSigninButton
-  style={{ width: 192, height: 48 }}
-  size={GoogleSigninButton.Size.Wide}
-  color={GoogleSigninButton.Color.Dark}
-  onPress={() => signIn()}
-  // disabled={this.state.isSigninInProgress}
-/>         
-         
-          <TouchableOpacity
-          // () => Linking.openURL('http://localhost:3001/auth/login/google')
-            //  onPress={() => setShowLoginWebView(true)}>
-            onPress={() => Linking.openURL('http://localhost:3001/auth/login/google')}>
-             <Text style={styles.buttonText} >
-               {/* {data === "" ? "Connect via Google" : "You are connected !"}</Text> */}
-               Connect via Google
-               </Text>
-           </TouchableOpacity>
-          <Section title="Test">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={backgroundStyle}>
+        <Login></Login>
+          
+      </SafeAreaView>
+    </Provider>
   );
 };
 
